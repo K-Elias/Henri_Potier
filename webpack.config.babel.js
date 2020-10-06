759d5852364b76e59377e9a1d00e9313449c0310
Elias <elias.aboussair@outlook.com>
2020-10-06 02:22:29 +0200

    init project

---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const mode =
+	process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+export default {
+	mode,
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+		modules: ['node_modules', './src']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: './public/index.html',
+			filename: 'index.html',
+			scriptLoading: 'defer'
+		})
+	]
+};
